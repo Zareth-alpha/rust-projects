945290f145604f573d00a6004fdf1b70b4342e30
Jonathan Brown <jonathan.z.r.brown@gmail.com>
2022-02-19 21:41:03 -0800

    Finished Chapter 2.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
+//DEBUG LINE    println!("The secret number is: {}", secret_number);
 
     loop{
         println!("Please input your guess.");
@@ -35,14 +35,20 @@ fn main() {
             //Result's enum variants are OK or Err.
             //expect calls a result method. If err, display message passed in argument.
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small..."),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 
