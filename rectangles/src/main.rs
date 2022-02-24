6fd64e90371b3ec80ad9f07d3d410f68c36eaa01
Jonathan Brown <jonathan.z.r.brown@gmail.com>
2022-02-23 22:34:02 -0800

    Chapter 5 Complete

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,10 +8,25 @@ struct Rectangle {
     height: u32,
 }
 
+/*
+In the signature for area, instead of rectangle: &rectangle, the &self is actually short for self: &Self.
+Methods must have a parameter named self of type Self for their first parameter.
+Be careful of ownership. Giving ownership is usually reserved for transforming self into another type.
+
+*/
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    /*
+    All functions defined within an impl block are called associated functions because they’re associated with the type named after the impl. We can define associated functions that don’t have self as their first parameter (and thus are not methods) because they don’t need an instance of the type to work with. We’ve already used one function like this, the String::from function, that’s defined on the String type.
+    Associated functions that aren’t methods are often used for constructors that will return a new instance of the struct. For example, we could provide an associated function that would have one dimension parameter and use that as both width and height, thus making it easier to create a square Rectangle rather than having to specify the same value twice:
+    */
+
 }
 
 fn main() {
@@ -30,6 +45,10 @@ fn main() {
 //        area(&rect1)
         rect1.area()
     );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
 
     //takes ownership of expression, prints the file and line #
     //and the result of the expression.
